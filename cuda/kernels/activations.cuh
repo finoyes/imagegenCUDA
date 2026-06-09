8750f85a1c8d88c86e37f472d83b8239e8d8b7ef
finoyes <advaitkshirsagar228@gmail.com>
2026-06-09 19:55:28 +0530

    initialised GeLU and ReLU

--- cuda/kernels/activations.cuh
+++ cuda/kernels/activations.cuh
@@ -2,3 +2,10 @@
 // GeLU(x) = 0.5 * x * (1 + tanh(sqrt(2/pi) * (x + 0.044715 * x*x*x))
 //not ReLU cuz solves dying ReLU problem. leaky ReLU also solves it but not  as effectively.
 
+#pragma once
+#include <cuda_runtime.h>
+
+void GeLU_forward();
+void GeLU_backward();
+void ReLU_forward();
+void ReLU_backward();
