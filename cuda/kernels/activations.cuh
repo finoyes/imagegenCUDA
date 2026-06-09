710ddfe42767473493aa78dc540359307ba8cb51
finoyes <advaitkshirsagar228@gmail.com>
2026-06-09 19:51:48 +0530

    decribed GeLU.

--- cuda/kernels/activations.cuh
+++ cuda/kernels/activations.cuh
@@ -1,1 +1,4 @@
 //activation funtion kernel (GeLU).
+// GeLU(x) = 0.5 * x * (1 + tanh(sqrt(2/pi) * (x + 0.044715 * x*x*x))
+//not ReLU cuz solves dying ReLU problem. leaky ReLU also solves it but not  as effectively.
+
