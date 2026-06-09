8fc144369eeb071d2e878c810da5d9412dd202c6
finoyes <advaitkshirsagar228@gmail.com>
2026-06-09 21:50:41 +0530

    dilly dallying soo.. slow work.

--- cuda/kernels/activations.cuh
+++ cuda/kernels/activations.cuh
@@ -5,7 +5,7 @@
 #pragma once
 #include <cuda_runtime.h>
 
-void GeLU_forward();
-void GeLU_backward();
-void ReLU_forward();
-void ReLU_backward();
+void GeLU_forward(float* output, const float* input, int n);// n = total element count( batchxseqxd_model).
+void GeLU_backward(float* d_input, const float* d_output, const float* input, int n);// d_output(upstream gradient) and input(og forward, input to compute the derivative)
+void ReLU_forward(float* output, const float* input, int n);
+void ReLU_backward(float* d_input, const float* d_output, const float* input, int n);
