39abf704b15eece3f50cae418e02329ad917ec1b
finoyes <advaitkshirsagar228@gmail.com>
2026-06-10 04:11:34 +0530

    GeLU & ReLU activation.

--- cuda/kernels/activations.cu
+++ cuda/kernels/activations.cu
@@ -1,2 +1,58 @@
 // actual activationnnnn baby.
 
+#define GeLU_coeff 0.044715f
+#define SQRT_2_pi 0.7978845608f //sqrt(2/pi)
+
+__device__ inline float GeLU_val(float x){
+
+    float t = SQRT_2_pi * (x + GeLU_coeff * x * x * x);
+    return 0.5f * x * (1.0f + tanhf(t)); //tanhf(single precision tanh) faster than tanh((double)causes implicit widening to double precision) on GPU.
+
+}
+
+__device__ inline float GeLU_deriv(float x){
+
+float t = SQRT_2_pi * (x + GeLU_coeff * x * x * x);
+float tanh_t = tanhf(t);
+float sech2_t = 1.0f - tanh_t * tanh_t; //sech^2(t) = 1 - tanh^2(t)
+float dt_dx = SQRT_2_pi * (1.0f + 3.0f * GeLU_coeff * x * x);
+return 0.5f * (1.0f + tanh_t) + 0.5f * x * sech2_t * dt_dx;
+
+} // d(GeLU)/dx = 0.5 * (1 + tanh(t)) + 0.5 * x * sech^2(t) * dt/dx.
+
+__global__ void GeLU_fwd_kernel(float* output, const float* in, int n) {
+
+    int i = threadIdx.x * blockDim.x + threadIdx.x; //1d index formula convverts 2D grid position(block, thread) into a single flat index. => eg. 2B and 5Th, blockDim 256 -> i = 2*256 + 5 = 517.
+    if (i < n){
+       output[i] = GeLU_val(in[i]);
+    }
+
+}
+
+__global__ void GeLU_bwd_kernel(float* d_in, const float* d_out, const float* input, int n) {
+
+    int i = threadIdx.x * blockDim.x + threadIdx.x;
+    if (i < n){
+       d_in[i] = d_out[i] * GeLU_deriv(input[i]);
+    }
+
+}
+
+__global__ void ReLU_fwd_kernel(float* d_in, const float* d_out, const float* in, int n){
+
+    int i = threadIdx.x * blockDim.x + threadIdx.x;
+    if(i < n){
+      d_in[i] = (in[i] > 0.0f) ? d_out[i] : 0.0f; 
+    }
+
+}/* ReLU(x) = x>=0 => 1
+              x<=0 => 0
+ d(ReLU)/dx = x>=0 => 1 */
+
+ void GeLU_fwd(float* output, const float* input, int n){
+
+    int threads = 256;
+    GeLU_fwd_kernel<<<(n + threads - 1) / threads, threads>>>(output, input, n); //ceiling division inthe first kernel dimension equivalent to ceil(n/256).
+    //ensure enough bloacks to cover all n elements, even if not divisible by 256.
+
+ }
--- cuda/kernels/activations.cuh
+++ cuda/kernels/activations.cuh
@@ -1,5 +1,5 @@
 //activation funtion kernel (GeLU).
-// GeLU(x) = 0.5 * x * (1 + tanh(sqrt(2/pi) * (x + 0.044715 * x*x*x))
+// GeLU(x) = 0.5 * x * (1 + tanh(sqrt(2/pi * (x + 0.044715 * x*x*x)))
 //not ReLU cuz solves dying ReLU problem. leaky ReLU also solves it but not  as effectively.
 
 #pragma once
